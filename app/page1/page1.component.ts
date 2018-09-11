06fc0cda1452a9aaf721575d95628e009e344305
jprice <jprice@mac-jprice.cb.careerbuilder.com>
2018-09-11 09:00:28 -0700

    with clear history

--- app/page1/page1.component.ts
+++ app/page1/page1.component.ts
@@ -5,26 +5,26 @@ import { Store } from "@ngrx/store";
 import { State } from "~/home/store/home.reducer";
 
 @Component({
-	selector: "Page1",
-	moduleId: module.id,
-	templateUrl: "./page1.component.html",
+  selector: "Page1",
+  moduleId: module.id,
+  templateUrl: "./page1.component.html",
 })
 export class Page1Component implements OnInit, OnDestroy {
 
-	constructor(private store: Store<State>, private router: RouterExtensions) {
-		console.log('PAGE 1 --- COMPONENT CONSTRUCTOR!');
-	}
-
-	ngOnInit(): void {
-		console.log('PAGE 1 --- INIT!');
-	}
-
-	ngOnDestroy() {
-		console.log('PAGE 1 --- DESTROY!');
+  constructor(private store: Store<State>, private router: RouterExtensions) {
+    console.log('PAGE 1 --- COMPONENT CONSTRUCTOR!');
   }
-  
+
+  ngOnInit(): void {
+    console.log('PAGE 1 --- INIT!');
+  }
+
+  ngOnDestroy() {
+    console.log('PAGE 1 --- DESTROY!');
+  }
+
   addOne() {
     this.store.dispatch(new AddItem());
-    this.router.navigate(['home']);
+    this.router.navigate(['home'], { clearHistory: true });
   }
 }
